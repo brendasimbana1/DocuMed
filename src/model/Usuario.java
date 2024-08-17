7114bee0014ef99c3a0d576189baedab499ed3e6
Mateo Montenegro <emontenegroc@est.ups.edu.ec>
2024-08-16 22:32:49 -0500

    Creación de modelo Usuario

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,5 +1,37 @@
 package model;
 
 public class Usuario {
+	
+	private String nombre;
+	private String contrasenia;
+	
+	public Usuario(String nombre, String contrasenia) {
+		super();
+		this.nombre = nombre;
+		this.contrasenia = contrasenia;
+	}
 
+	public String getNombre() 
+	{
+		return nombre;
+	}
+
+	public void setNombre(String nombre) 
+	{
+		this.nombre = nombre;
+	}
+
+	public String getContrasenia() 
+	{
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) 
+	{
+		this.contrasenia = contrasenia;
+	}
+	
+	
+	
+	
 }
